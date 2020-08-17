89d13ddf1d0d848610b4aded1b71f89fd5503a6c
Your Name <you@example.com>
2020-08-17 04:36:52 +0100

    User details update

--- lib/Screens/AllMembers/userDetails.dart
+++ lib/Screens/AllMembers/userDetails.dart
@@ -17,63 +17,73 @@ class _DetailPageState extends State<DetailPage> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-        
-        appBar: AppBar(
-          title: Text(
-            widget.member.data["surname"].toString(),
-          ),
+      appBar: AppBar(
+        title: Text(
+          widget.member.data["surname"].toString(),
         ),
-        
-        body: Column(
-          children: <Widget>[
-            Row(
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 230,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/hands_up.jpg'),
+                fit: BoxFit.fill,
+              ),
+            ),
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.only(top: 20.0),
-                  child: new Stack(fit: StackFit.loose, children: <Widget>[
-                    new Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        ClipOval(
-                          child: SizedBox(
-                            width: 140,
-                            height: 140,
-                            child: Image.network(
-                                widget.member.data['downloadUrl'].toString()),
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: new Stack(
+                    fit: StackFit.loose,
+                    children: <Widget>[
+                      new Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          ClipOval(
+                            child: SizedBox(
+                              width: 200,
+                              height: 200,
+                              child: Image.network(
+                                  widget.member.data['downloadUrl'].toString()),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                        padding: EdgeInsets.only(top: 90.0, right: 100.0),
-                        child: new Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            new CircleAvatar(
-                              backgroundColor: kPrimaryColor,
-                              radius: 25.0,
-                              child: InkResponse(
-                                onTap: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => GenerateScreen(
-                                        timestamp: widget.member.data['createdAt'].toString(),
+                        ],
+                      ),
+                      Padding(
+                          padding: EdgeInsets.only(top: 140.0, right: 0),
+                          child: new Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              new CircleAvatar(
+                                backgroundColor: kPrimaryColor,
+                                radius: 25.0,
+                                child: InkResponse(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => GenerateScreen(
+                                          timestamp: widget
+                                              .member.data['createdAt']
+                                              .toString(),
+                                        ),
                                       ),
-                                    ),
-                                  );
-                                  print('tapp tapp...............');
-                                },
-                                child: Image.network(
-                                    'https://cdn0.iconfinder.com/data/icons/material-style/48/qrcode-512.png'),
-                              ),
-                            )
-                          ],
-                        )),
-                    Padding(
-                        padding: EdgeInsets.only(top: 90.0, left: 90.0),
+                                    );
+                                    print('tapp tapp...............');
+                                  },
+                                  child: Image.network(
+                                      'https://cdn0.iconfinder.com/data/icons/material-style/48/qrcode-512.png'),
+                                ),
+                              )
+                            ],
+                          )),
+                      Padding(
+                        padding: EdgeInsets.only(top: 140.0, left: 150.0),
                         child: new Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
@@ -90,37 +100,106 @@ class _DetailPageState extends State<DetailPage> {
                             )
                           ],
                         ),
-                        ),
-                  ],
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
-            SizedBox(
-              height: 20,
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 0),
+            padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+            width: size.width * 1,
+            height: 430,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/data_model.jpg'),
+                fit: BoxFit.fill,
+              ),
             ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10),
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-              width: size.width * 0.9,
-              decoration: BoxDecoration(
-                color: kPrimaryLightColor,
-                borderRadius: BorderRadius.circular(5),
+            child: Container(
+              child: Column(
+                children: <Widget>[
+//Tab here
+                  Container(
+                    child: DefaultTabController(
+                      length: 2,
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 0, horizontal: 5),
+                            child: Container(
+                              height: 40,
+                              decoration: new BoxDecoration(
+                                  color: Theme.of(context).primaryColor,
+                                  borderRadius: BorderRadius.circular(50)),
+                              child: new TabBar(
+                                //indicatorSize: TabBarIndicatorSize.label,
+                                unselectedLabelColor: Colors.white,
+
+                                indicatorSize: TabBarIndicatorSize.tab,
+                                indicator: BoxDecoration(
+                                    gradient: LinearGradient(colors: [
+                                      Colors.black,
+                                      Colors.grey[850]
+                                    ]),
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Colors.redAccent),
+                                tabs: [
+                                  new Tab(
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        'Details',
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                  ),
+                                  new Tab(
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        'Activities',
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SingleChildScrollView(
+                            child: Container(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 5),
+                                child: Container(
+                                  color: Colors.black.withOpacity(0.8),
+                                  height: 370,
+                                  child: TabBarView(
+                                    children: [
+                                      Icon(Icons.movie),
+                                      Icon(Icons.games),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              child: Text(
-                widget.member.data['surname'].toString() +
-                    ' ' +
-                    widget.member.data['firstname'].toString(),
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            )
-          ],
-        ),
-        );
+
+//Build details here
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
